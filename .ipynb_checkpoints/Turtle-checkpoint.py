59a91bb2cfc585bc71ef4873414ce02963aa015b
Nathaniel Solon Pedersen <nasope@gmail.com>
2023-09-11 18:44:22 +0200

    init

---
+++ .ipynb_checkpoints/Turtle-checkpoint.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.speed(20)
+
+for j in range(1000):
+    for i in range(4):
+        turtle.forward(30)
+        turtle.left(20)
+        turtle.forward(40)
+        turtle.right(50)
+    turtle.right(10)
+turtle.done()
---
+++ README.md
@@ -0,0 +1,2 @@
+# DAT120
+Consists of every Practice we have had in the subject DAT120
